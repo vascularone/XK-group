2dca67e28dc3909f45d661c4b4792fd8ee9b1940
vascularone <rinorkastrati46@gmail.com>
2023-02-28 16:10:17 +0100

    landing page created

--- src/components/pages/landingpage.js
+++ src/components/pages/landingpage.js
@@ -1,23 +1,50 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Landing = () => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/shippingpage");
+    }
     return (
         <div className="wrapper">
-            <section className="firstSection">
-                <div className="shampoos">
+            <section className="navbar">
+                <div className="navbar-text">
                     <img src="glissHairPro.png" alt="no gliss" className="gliss" />
-                    <span className="shampooText">SHAMPOOS</span>
-                    <img src="allShampoos12.png" alt="no shampoos" className="allShampoos" />
+                    <div className="breaking-line"></div>
+                    <span className="primaryText">HEALTHY HAIR</span>
+                    <span className="secondaryText">TO BE CONFIDENT</span>
                 </div>
+                <img src="allAQUA3.png" alt="shampoos not responding" className="shampoos" />
+                <div className="horizontal-line"></div>
             </section>
-            <section className="secondSection">
-                <div className="conditioners">
-                    <span className="conditionerText">CONDITIONERS</span>
-                    <img src="conditioners.png" alt="no conditioners" className="allConditioners" />
-                </div>
-            </section>
-            <section className="thirdSection">
 
+            <section className="firstSection">
+                <img src="aquaSplash.png" alt="aqua not responding" className="aquaShampoo" />
+                <div className="shampooDescription">
+                    <span className="secondaryText">
+                        <strong>Schwarzkopf Gliss Aqua Revive</strong> moisturizes and visibly improves the quality of your hair, without weighing it down!</span>
+                    <button onClick={handleClick} className="aquaCheckout"><span>Buy now</span></button>
+                </div>
+            </section>
+
+            <section className="secondSection">
+                <div className="conditionerDescription">
+                    <span className="secondaryText">
+                        <strong>Schwarzkopf Gliss Aqua Revive</strong> moisturizes and visibly improves the quality of your hair, without weighing it down!</span>
+                    <button onClick={handleClick} className="aquaCheckout"><span>Buy now</span></button>
+                </div>
+                <img src="conditionerSplash2.png" alt="aqua not responding" className="aquaConditioner" />
+            </section>
+
+            <section className="thirdSection">
+                <img src="aquaSpray.png" alt="aqua not responding" className="aquaSpray" />
+                <div className="sprayDescription">
+                    <span className="secondaryText">
+                        <strong>Schwarzkopf Gliss Aqua Revive</strong> moisturizes and visibly improves the quality of your hair, without weighing it down!</span>
+                    <button onClick={handleClick} className="aquaCheckout"><span>Buy now</span></button>
+                </div>
             </section>
         </div>
     );
--- src/components/pages/shippingpage.js
+++ src/components/pages/shippingpage.js
@@ -1,13 +1,48 @@
-import React from "react";
+import { useState } from "react";
+import $ from "jquery";
 
 const Shipping = () => {
+    const [name, setName] = useState("");
+    const [result, setResult] = useState("");
+
+    const handleChange = (e) => {
+        setName(e.target.value);
+    };
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        const form = $(e.target);
+        $.ajax({
+            type: "POST",
+            url: form.attr("action"),
+            data: form.serialize(),
+            success(data) {
+                setResult(data);
+            },
+        });
+    };
+
     return (
-        <div>
-            <h1>
-                This is the Shipping
-            </h1>
+        <div className="App">
+            <form
+                action="http://localhost:8000/server.php"
+                method="post"
+                onSubmit={(event) => handleSumbit(event)}
+            >
+                <label htmlFor="name">Name: </label>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={name}
+                    onChange={(event) => handleChange(event)}
+                />
+                <br />
+                <button type="submit">Submit</button>
+            </form>
+            <h1>{result}</h1>
         </div>
     );
-};
+}
 
 export default Shipping;
